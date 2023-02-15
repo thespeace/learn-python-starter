c76f8ea5c78d07ce91e81883add5dea7595726d8
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-02-15 23:42:01 +0900

    230215 23:41 learn variables and datatype

---
+++ 01_variables_functions/01_Variables_and_Data_type.py
@@ -0,0 +1,25 @@
+#number
+
+a = 2
+b = 3
+c = a + b
+
+print(c)
+#> 5
+c = 1
+print(c) #위에서 아래로 실행된다.
+#> 1
+
+my_age = 91 # "snake_case"를 사용하는 관습이 있다. <-> JS의 "camelCase"
+
+#string
+my_name = "seo"
+
+#boolean
+my_job = True
+#my_job = False
+age = 31
+
+print("Hello my name is",my_name)
+print("and I'm", age, " years old")
+print(True, "hello", 12)
