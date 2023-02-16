d9a2f63664395d7ef085fb9e265cba81b717d444
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-02-16 23:32:46 +0900

    230216 23:32 regular commit

---
+++ 03_data_structures/01_methods.py
@@ -0,0 +1,14 @@
+#========Method : 데이터 뒤에 결합/연결된 function.=========
+name = "seo" #String 내부에는 많은 Method을 내포하고 있다.
+print(name.upper()) #텍스트를 대문자로 만들어 준다.
+print(name.capitalize()) #텍스트의 첫글자만 대문자로 만들어 준다.
+print(name.startswith("s")) #첫글자를 체크해서 boolean으로 반환 해준다.
+print(name.replace("o","❤")) #해당 글자를 찾아서 원하는 글자로 대체 해준다.
+#이처럼 데이터를 가지고 method를 통해 데이터를 변환하고 조작할 수 있다.
+
+#=======list=======
+#쉼표를 사용해서 데이터를 분리한다.
+days_of_week = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"]
+print(days_of_week)
+
+
---
+++ 03_data_structures/README.md
@@ -0,0 +1,8 @@
+## 자료 구조(data structure)?
+### 데이터를 구조화할 때 사용합니다.
+
+## 파이썬의 데이터 구조 3가지
+1. list
+2. tuple
+3. dictionary
+
