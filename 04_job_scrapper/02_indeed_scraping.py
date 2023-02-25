7ea6a95231e5a4cc30637a36528d5e5720637c66
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-02-25 13:27:47 +0900

    230225 13:26 using selenium for 403 fix

--- 04_job_scrapper/02_indeed_scraping.py
+++ 04_job_scrapper/02_indeed_scraping.py
@@ -1,5 +1,14 @@
 from requests import get
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True) #브라우저 꺼짐 방지 코드
+
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = chrome_options) #크롬드라이버를 최신으로 유지해줍니다.
 
 base_url = "https://kr.indeed.com/jobs?q="
 search_term = "python"
