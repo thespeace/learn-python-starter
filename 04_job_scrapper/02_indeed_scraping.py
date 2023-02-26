72fc712cc9fb13a4d60802cc1a3015d9762ce693
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-02-26 18:11:24 +0900

    230226 18:09 Counting pages and scraping all indeed jobs

--- 04_job_scrapper/02_indeed_scraping.py
+++ 04_job_scrapper/02_indeed_scraping.py
@@ -1,21 +1,7 @@
-from requests import get
-# from bs4 import BeautifulSoup
-from selenium.webdriver.chrome import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from extractors.indeed import extract_indeed_jobs
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True) #브라우저 꺼짐 방지 코드
+keyword = input("What do you want to search for?")
+jobs = extract_indeed_jobs(keyword)
 
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = chrome_options) #크롬드라이버를 최신으로 유지해줍니다.
-
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
-
-response = get(f"{base_url}{search_term}")
-
-if response.status_code != 200:
-    print("Cant request page")
-else:
-    print(response.text)
+for job in jobs:
+    print(job)
